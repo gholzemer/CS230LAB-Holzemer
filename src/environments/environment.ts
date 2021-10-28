f06dbf72829e446dc0ccb5145d2051d54ae4655d
gholzemer <gh0026@mix.wvu.edu>
2021-10-28 14:38:58 -0400

    Lab 9 - New dbService

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:
+    {
+
+      apiKey: "AIzaSyD_6AeCDhnjDkazKeZ_K8Rh0HYeKRbPosU",
+    
+      authDomain: "razer-75b9b.firebaseapp.com",
+    
+      databaseURL: "https://razer-75b9b-default-rtdb.firebaseio.com",
+    
+      projectId: "razer-75b9b",
+    
+      storageBucket: "razer-75b9b.appspot.com",
+    
+      messagingSenderId: "420470922923",
+    
+      appId: "1:420470922923:web:d2d95bc459359e09a321ee"
+    
+    }
+  
 };
 
 /*
