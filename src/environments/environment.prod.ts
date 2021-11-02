cff2d39a43b0f7f63e11905b2a2d8f66ad38bc73
gholzemer <gh0026@mix.wvu.edu>
2021-11-02 13:58:33 -0400

    Firebase Hosting Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebaseConfig:
+    {
+
+      apiKey: "AIzaSyD_6AeCDhnjDkazKeZ_K8Rh0HYeKRbPosU",
+    
+      authDomain: "razer-75b9b.firebaseapp.com",
+    
+      databaseURL: "https://razer-75b9b-default-rtdb.firebaseio.com",
+    
+      projectId: "razer-75b9b",
+    
+      storageBucket: "razer-75b9b.appspot.com",
+    
+      messagingSenderId: "420470922923",
+    
+      appId: "1:420470922923:web:d2d95bc459359e09a321ee"
+    
+    }
 };
